aba66ac2046180dc6aea3347c1f8009950662554
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-12-06 16:51:42 -0300

    Update KeyBinds.hx

--- source/KeyBinds.hx
+++ source/KeyBinds.hx
@@ -18,23 +18,23 @@ class KeyBinds
     public static function keyCheck():Void
     {
         if(FlxG.save.data.upBind == null){
-            FlxG.save.data.upBind = "W";
+            FlxG.save.data.upBind = "D";
             trace("No UP");
         }
         if(FlxG.save.data.downBind == null){
-            FlxG.save.data.downBind = "S";
+            FlxG.save.data.downBind = "F";
             trace("No DOWN");
         }
         if(FlxG.save.data.leftBind == null){
-            FlxG.save.data.leftBind = "A";
+            FlxG.save.data.leftBind = "J";
             trace("No LEFT");
         }
         if(FlxG.save.data.rightBind == null){
-            FlxG.save.data.rightBind = "D";
+            FlxG.save.data.rightBind = "K";
             trace("No RIGHT");
         }
         if(FlxG.save.data.killBind == null){
-            FlxG.save.data.killBind = "R";
+            FlxG.save.data.killBind = "NONE";
             trace("No KILL");
         }
     }
