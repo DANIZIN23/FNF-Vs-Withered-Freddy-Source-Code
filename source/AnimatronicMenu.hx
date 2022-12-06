9de83bd74a4e4e95d239a0f84ad58ca1411f015f
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-12-06 17:39:35 -0300

    Update AnimatronicMenu.hx

--- source/AnimatronicMenu.hx
+++ source/AnimatronicMenu.hx
@@ -326,11 +326,9 @@ class AnimatronicMenu extends MusicBeatState
 
 		
 		
-		#if polymod
-		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']});
-		#end
 		
-		#if sys
+		
+		#if (windows && html5
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
 			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
 		#end
